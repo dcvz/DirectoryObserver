7d547041ead1782e117b9a613f29c13b83bf94f2
David Chavez <david@8fit.com>
2016-07-01 13:20:49 +0200

    Add support for Swift3

--- DirectoryObserver/Classes/DirectoryObserver.swift
+++ DirectoryObserver/Classes/DirectoryObserver.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 private struct Constants {
-    static let pollInterval: NSTimeInterval = 0.2
+    static let pollInterval = 0.2
     static let pollRetryCount = 5
 }
 
@@ -17,28 +17,28 @@ public class DirectoryObserver {
 
     // MARK: - Errors
 
-    public enum Error: ErrorType {
-        case AlreadyObserving, FailedToStartObserver, FailedToStopObserver
+    public enum Error: ErrorProtocol {
+        case AlreadyObserving, FailedToStartObserver, FailedToStopObserver, InvalidPath
     }
 
     // MARK: - Attributes
 
-    public let watchedPath: NSURL
+    public let watchedPath: URL
     private(set) var isObserving = false
 
 
     // MARK: - Attributes (Private)
 
     private let completionHandler: (() -> Void)
-    private var queue = dispatch_queue_create("DCDirectoryWatcherQueue", .None)
+    private var queue = DispatchQueue(label: "directory_observer_queue")
     private var retriesLeft = Constants.pollRetryCount
     private var isDirectoryChanging = false
-    private var source: dispatch_source_t?
+    private var source: DispatchSourceFileSystemObject?
 
 
     // MARK: - Initializers
 
-    public init(pathToWatch path: NSURL, callback: () -> Void) {
+    public init(pathToWatch path: URL, callback: () -> Void) {
         watchedPath = path
         completionHandler = callback
     }
@@ -54,48 +54,41 @@ public class DirectoryObserver {
             throw Error.AlreadyObserving
         }
 
+        guard let path = watchedPath.path else {
+            throw Error.InvalidPath
+        }
+
         // Open an event-only file descriptor associated with the directory
         let fd: CInt = open(watchedPath.path!, O_EVTONLY)
         if fd < 0 { throw Error.FailedToStartObserver }
 
-        let cleanup: dispatch_block_t = { close(fd) }
+        let cleanup: () -> Void = { close(fd) }
 
         // Get a low priority queue
-        let queue: dispatch_queue_t = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)
+        let queue = DispatchQueue.global(attributes: [.priorityLow])
 
         // Monitor the directory for writes
-        source = dispatch_source_create(
-            DISPATCH_SOURCE_TYPE_VNODE, // Monitors a file descriptor
-            UInt(fd), // our file descriptor
-            DISPATCH_VNODE_WRITE, // The file-system object data changed.
-            queue // the queue to dispatch on
-        )
+        source = DispatchSource.fileSystemObject(fileDescriptor: fd, eventMask: .write, queue: queue)
 
-        if let source = source {
-            // Call directoryDidChange on event callback
-            dispatch_source_set_event_handler(source) { [weak self] in
-                self?.directoryDidChange()
-            }
-
-            // Dispatch source destructor
-            dispatch_source_set_cancel_handler(source, cleanup)
-
-            // Sources are create in suspended state, so resume it
-            dispatch_resume(source)
-        } else {
-            cleanup()
-            throw Error.FailedToStartObserver
+        // Call directoryDidChange on event callback
+        source!.setEventHandler() { [weak self] in
+            self?.directoryDidChange()
         }
 
+        // Dispatch source destructor
+        source!.setCancelHandler(handler: cleanup)
+
+        // Sources are create in suspended state, so resume it
+        source!.resume()
         isObserving = true
     }
 
 
     /// Stops the observer
-    public func stopObserving() throws {
-        if let source = source {
-            dispatch_source_cancel(source)
-            throw Error.FailedToStopObserver
+    public func stopObserving() {
+        if source != nil {
+            source!.cancel()
+            source = nil
         }
 
         isObserving = false
@@ -114,22 +107,21 @@ public class DirectoryObserver {
 
     private func checkForChangesAfterDelay() {
         let metadata: [String] = directoryMetadata()
-        let popTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(Constants.pollInterval * Double(NSEC_PER_SEC)))
-        dispatch_after(popTime, queue) { [weak self] in
+        queue.after(when: DispatchTime.now() + Constants.pollInterval) { [weak self] in
             self?.pollDirectoryForChanges(metadata: metadata)
         }
     }
 
     private func directoryMetadata() -> [String] {
-        let fm = NSFileManager.defaultManager()
-        let contents = try? fm.contentsOfDirectoryAtURL(watchedPath, includingPropertiesForKeys: nil, options: [])
+        let fm = FileManager.default()
+        let contents = try? fm.contentsOfDirectory(at: watchedPath, includingPropertiesForKeys: nil, options: [])
         var directoryMetadata: [String] = []
 
         if let contents = contents {
             for file in contents {
                 autoreleasepool {
-                    if let fileAttributes = try? fm.attributesOfItemAtPath(file.path!) {
-                        let fileSize = fileAttributes[NSFileSize] as! Int
+                    if let fileAttributes = try? fm.attributesOfItem(atPath: file.path!) {
+                        let fileSize = fileAttributes[FileAttributeKey.size.rawValue] as! Int
                         let fileHash = "\(file.lastPathComponent!)\(fileSize)"
                         directoryMetadata.append(fileHash)
                     }
@@ -150,7 +142,7 @@ public class DirectoryObserver {
             retriesLeft -= 1
             checkForChangesAfterDelay()
         } else {
-            dispatch_async(dispatch_get_main_queue()) { [weak self] in
+            DispatchQueue.main.async() { [weak self] in
                 self?.completionHandler()
             }
         }
